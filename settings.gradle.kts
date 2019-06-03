467a85153e6dd8537198ee5ac34e13ac68b633d2
Sergey Rostov <sergey.rostov@jetbrains.com>
2019-06-03 15:28:28 +0300

    Example for new kotlin 1.3.40 org.jetbrains.kotlin.js plugin.
    See https://youtrack.jetbrains.com/issue/KT-31743 for more details.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,14 +2,11 @@ pluginManagement {
     resolutionStrategy {
         repositories {
             mavenCentral()
-            maven {
-                url = uri("https://dl.bintray.com/kotlin/kotlin-eap")
-            }
+            maven { url = uri("https://dl.bintray.com/kotlin/kotlin-eap") }
         }
         eachPlugin {
             when (requested.id.id) {
-                "kotlin2js" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
-                "org.jetbrains.kotlin.frontend" -> useModule("org.jetbrains.kotlin:kotlin-frontend-plugin:${requested.version}")
+                "org.jetbrains.kotlin.js" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
             }
         }
     }
