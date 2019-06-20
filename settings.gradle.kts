a51b3a010aa8deebf94cc1280d4b62ece8a51450
ilgonmic <ilgonmic@gmail.com>
2019-06-20 09:49:04 +0300

    Remove redundant

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,3 @@
-pluginManagement {
-    resolutionStrategy {
-        repositories {
-            mavenCentral()
-            maven { url = uri("https://dl.bintray.com/kotlin/kotlin-eap") }
-        }
-        eachPlugin {
-            when (requested.id.id) {
-                "org.jetbrains.kotlin.js" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
-            }
-        }
-    }
-}
 rootProject.name = "kotlinjs-multi-module"
 include("lib")
 include("app")
